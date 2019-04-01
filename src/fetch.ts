ff01580628fde6869b0feb923832840650c9e145
Gordon <gburgett@watermark.org>
2019-04-01 15:03:00 -0500

    Unify 'fetch' and 'parse' under one semaphore lock

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,8 +1,20 @@
 import { ParallelTransform, ParallelTransformOptions } from 'async-toolbox/stream'
 require('es6-promise/auto')
+import { ReadLock } from 'async-toolbox'
+import { Response } from 'node-fetch'
+import { Writable } from 'stream'
 import fetch from 'universal-fetch'
+import { Result } from './model'
+import { CheerioParser } from './parsers/cheerio-parser'
+import { RegexpParser } from './parsers/regexp-parser'
 import { EOF, isEOF } from './reentry'
 import { URL } from './url'
+
+interface Parsers {
+  [mimeType: string]: {
+    parse: (response: Response, push: (result: URL) => void) => Promise<Result>,
+  }
+}
 
 export interface FetchOptions extends ParallelTransformOptions {
   objectMode?: true
@@ -10,10 +22,14 @@ export interface FetchOptions extends ParallelTransformOptions {
   hostnames: Set<string>
 
   acceptMimeTypes?: string[]
+
+  reentry?: Writable
+  parsers?: Parsers
 }
 
 export class Fetch extends ParallelTransform {
   private _acceptMimeType: string
+  private _parsers: Parsers
 
   constructor(private readonly options: FetchOptions) {
     super(Object.assign({},
@@ -25,10 +41,16 @@ export class Fetch extends ParallelTransform {
     this._acceptMimeType = options.acceptMimeTypes ?
       options.acceptMimeTypes.join(', ') :
       'text/html, application/json'
+
+    this._parsers = options.parsers || {
+        'default': new RegexpParser(),
+        'text/html': new CheerioParser(),
+      } as Parsers
   }
 
-  public async _transformAsync(url: URL | EOF): Promise<void> {
+  public async _transformAsync(url: URL | EOF, lock: ReadLock): Promise<void> {
     if (isEOF(url)) {
+      await lock.upgrade()
       this.push(url)
       return
     }
@@ -45,10 +67,21 @@ export class Fetch extends ParallelTransform {
       redirect: 'follow',
     })
 
-    this.push({
+    const chunk = {
       url,
       method,
       response,
+    }
+
+    const contentType = chunk.response.headers.get('content-type')
+    const parser = this._parsers[contentType || 'default'] ||
+      this._parsers.default ||
+      new RegexpParser()
+
+    const result = await parser.parse(chunk.response, (u) => {
+      console.log('TODO: reentry on URL', u)
     })
+
+    this.push(result)
   }
 }
