6675ef59e8fa4ee97cb6a9afc863e2a94e7d88b3
Gordon <gburgett@watermark.org>
2019-06-13 10:14:37 -0500

    Set proper base URL in HTML parsing

--- src/parsers/cheerio-parser.ts
+++ src/parsers/cheerio-parser.ts
@@ -6,13 +6,14 @@ import { parseUrl, URL } from '../url'
 
 export class CheerioParser {
   public async parse(response: Response, request: Request, push: (urls: URL) => void): Promise<void> {
-    const $ = cheerio.load(await response.text())
+    const text = await response.text()
+    const $ = cheerio.load(text)
 
     $('a[href]').each((index, anchorTag) => {
       const href = $(anchorTag).attr('href')
       if (href) {
         try {
-          const url = parseUrl(href, response.url)
+          const url = parseUrl(href, response.url || request.url)
           if (['http:', 'https:'].includes(url.protocol)) {
             push(url)
           }
